073e6f85172e89ea1f0f6c9c9ba50409ae895608
Petr Filaretov <p.filaretov@okko.tv>
2023-10-25 10:38:38 +0300

    initial commit - spring boot

---
+++ src/test/kotlin/dev/pfilaretov42/spring/dependency/defaultvalue/SpringDependencyWithDefaultValueApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.pfilaretov42.spring.dependency.defaultvalue
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDependencyWithDefaultValueApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

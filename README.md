3ac810b01228c6f838d3c00745af1696b4ce3441
Petr Filaretov <p.filaretov@okko.tv>
2023-10-26 22:44:20 +0300

    default value for injected dependencies

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-dependency-with-default-value
+
 Potential issues with injecting dependencies with default value in Spring
--- src/main/kotlin/dev/pfilaretov42/spring/dependency/defaultvalue/SpringDependencyWithDefaultValueApplication.kt
+++ src/main/kotlin/dev/pfilaretov42/spring/dependency/defaultvalue/SpringDependencyWithDefaultValueApplication.kt
@@ -1,11 +1,44 @@
 package dev.pfilaretov42.spring.dependency.defaultvalue
 
+import jakarta.annotation.PostConstruct
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class SpringDependencyWithDefaultValueApplication
 
 fun main(args: Array<String>) {
     runApplication<SpringDependencyWithDefaultValueApplication>(*args)
+}
+
+@Component
+class FordPrefect(
+    private val humans: List<Human> = emptyList(),
+) {
+    @PostConstruct
+    fun init() {
+        log.info("humans: $humans")
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(FordPrefect::class.java)
+    }
+}
+
+interface Human
+
+@Component
+class ArthurDent(
+    private val fordPrefect: FordPrefect,
+) : Human {
+    override fun toString() = "Arthur"
+}
+
+@Component
+class TrishaMcMillan(
+    private val fordPrefect: FordPrefect,
+) : Human {
+    override fun toString() = "Trisha"
 }
